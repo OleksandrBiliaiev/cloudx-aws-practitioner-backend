9779268720c1ac9f5c1c59f91a56ac71ef35f89f
Oleksandr Biliaiev <oleksandr_Biliaiev@epam.com>
2023-03-13 15:54:59 +0100

    log csv records and moving file to parsed dir;

--- import-service/importFileParser.js
+++ import-service/importFileParser.js
@@ -8,33 +8,37 @@ export const importFileParser = async (event) => {
   try {
     for (const record of event.Records) {
       const objectKey = record.s3.object.key;
-      console.log('objectKey:', objectKey)
-
       const s3Stream = s3.getObject({
         Bucket: BUCKET,
         Key: objectKey
       }).createReadStream();
 
-      s3Stream.pipe(csv())
-        .on('data', data => {
-          console.log('csv record: ', data);
-        })
-        .on('end', async () => {
+      await new Promise((resolve) => {
+        s3Stream
+          .pipe(csv())
+          .on('data', (data) => {
+            console.log('CSV record:', data);
+          })
+          .on('end', async () => {
+            await s3.copyObject({
+              Bucket: BUCKET,
+              CopySource: `${BUCKET}/${objectKey}`,
+              Key: objectKey.replace('uploaded', 'parsed')
+            }).promise();
 
-          await s3.copyObject({
-            Bucket: BUCKET,
-            CopySource: `${BUCKET}/${objectKey}`,
-            Key: objectKey.replace('uploaded', 'parsed')
-          }).promise();
+            await s3.deleteObject({
+              Bucket: BUCKET,
+              Key: objectKey
+            }).promise();
 
-          await s3.deleteObject({
-            Bucket: BUCKET,
-            key: objectKey
-          }).promise();
-        });
+            resolve({
+              statusCode: 200,
+              body: JSON.stringify({ message: 'Csv file successfully received and parsed.' })
+            })
+          })
+      })
     };
   } catch (error) {
     console.log(error);
   }
-
 };
